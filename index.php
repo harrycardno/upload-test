4205adad1b47aa1e0cfd7c3cecf8cf43126a1602
Harry Cardno <harry.cardno@44778-501.auckland.natcoll.ac.nz>
2015-06-09 16:57:15 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php # index.php
+
+	#1. Load libraries
+	
+	require_once 'form.lib.php';
+	require_once 'upload.lib.php';
+	require_once 'url.lib.php';
+
+	#2. Logic
+	// print_r ($_FILES);
+
+	# if any files were uploaded
+	if($_FILES){
+
+		# get the temp name (tmp) and the file name		
+		$tmp = $_FILES['file']['tmp_name'][0];
+		$filename = $_FILES['file']['name'][0];
+
+		#move the files into the "uploads" folder
+		move_uploaded_file($tmp, 'uploads/'.$filename);
+
+		#redirect to the newly uploaded file
+		URL::redirect('uploads/'.$filename);
+
+	}
+
+	#3. Load views
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Upload files with PHP</title>
+</head>
+<body>
+
+	<h1>Upload files with PHP</h1>
+	
+<!-- 	# upload forms have to use the post method -->
+
+	<?= Form::open_upload() ?>
+
+	<?= Form::max_file_size()?>
+
+		<div class="form_group">
+			<?=Form::label('file', 'File') ?>
+			<?=Form::file() ?>
+	
+		</div>
+		
+		<?= Form::submit() ?>
+
+	<?= Form::close() ?>
+
+
+	
+</body>
+</html>
